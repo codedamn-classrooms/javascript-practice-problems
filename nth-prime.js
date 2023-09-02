f32724a1d32bc17834e8b51d58c6905d34e19102
Mehul Mohan <mehulmpt@gmail.com>
2023-09-02 20:29:31 +0530

    Add files via script

--- nth-prime.js
+++ nth-prime.js
@@ -1,8 +1,8 @@
 //
-// This is only a SKELETON file for the 'Accumulate' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Nth Prime' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const accumulate = () => {
+export const prime = () => {
   throw new Error('Remove this statement and implement this function');
 };
