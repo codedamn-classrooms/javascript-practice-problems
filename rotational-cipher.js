61e7706e78e693b8b3cdfa52d04596d60d7fd54b
Mehul Mohan <mehulmpt@gmail.com>
2023-09-02 20:45:17 +0530

    Add files via script

--- rotational-cipher.js
+++ rotational-cipher.js
@@ -1,8 +1,8 @@
 //
-// This is only a SKELETON file for the 'Accumulate' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Rotational Cipher' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const accumulate = () => {
+export const rotate = () => {
   throw new Error('Remove this statement and implement this function');
 };
