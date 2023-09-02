758e8d46c82fcdb8ee70240884bbb290f2d5248b
Mehul Mohan <mehulmpt@gmail.com>
2023-09-02 20:19:12 +0530

    Add files via script

--- flatten-array.js
+++ flatten-array.js
@@ -1,8 +1,8 @@
 //
-// This is only a SKELETON file for the 'Accumulate' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Flatten Array' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const accumulate = () => {
+export const flatten = () => {
   throw new Error('Remove this statement and implement this function');
 };
