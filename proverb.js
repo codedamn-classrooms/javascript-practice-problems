d4343b60d3951c1b0663c759a99341f1a09b552a
Mehul Mohan <mehulmpt@gmail.com>
2023-09-02 20:35:58 +0530

    Add files via script

--- proverb.js
+++ proverb.js
@@ -1,8 +1,8 @@
 //
-// This is only a SKELETON file for the 'Accumulate' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Proverb' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const accumulate = () => {
+export const proverb = () => {
   throw new Error('Remove this statement and implement this function');
 };
