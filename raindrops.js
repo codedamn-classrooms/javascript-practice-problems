73d465235261bdfec3bfbd0f48ae897c7e332969
Mehul Mohan <mehulmpt@gmail.com>
2023-09-02 20:38:17 +0530

    Add files via script

--- raindrops.js
+++ raindrops.js
@@ -1,8 +1,8 @@
 //
-// This is only a SKELETON file for the 'Accumulate' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Raindrops' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const accumulate = () => {
+export const convert = () => {
   throw new Error('Remove this statement and implement this function');
 };
