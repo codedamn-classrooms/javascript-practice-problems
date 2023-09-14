063e824a3149209331471892697fdfd0ec2f76d9
Pranav <pranav.techiegeek@gmail.com>
2023-09-14 12:12:12 +0530

    sample tests

--- sum-of-multiples.js
+++ sum-of-multiples.js
@@ -7,3 +7,7 @@
 export const getEnergyPoints = (magicalItems, level) => {
 	// write your code here
 };
+
+// Sample Tests
+console.log(getEnergyPoints([3, 5], 20));
+console.log(getEnergyPoints([0], 1));
